6ae2e458305c18be3fbbd9a8e51de9604d92b1ba
Pavel <p.yudenko@gmail.com>
2023-05-06 13:44:25 +0300

    refactor: refactor solution

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,24 +6,30 @@ const path = require('path');
 const readfile = (filename) => {
   const greeting = 'Hello, I am C-3PO, human cyborg relations.\nFill my memory with better words, please...\n';
   const parting = '...and taking one last look... at my friends\n';
-  const closeConsole = () => {
-    rl.close();
-    output.write(parting);
-  };
 
   const rl = readline.createInterface({ input, output });
   const filepath = path.join(__dirname, filename);
   const stream = createWriteStream(filepath);
-  
-  rl.write(greeting);
-  rl.on('line', (input) => {
-    const writeableInput = input.trim().toLowerCase() === 'exit' ? closeConsole() : input;
-    stream.write(`${writeableInput}\n`);
-  });
-  rl.on('SIGINT', () => {
+
+  const handleClose = () => {
     rl.close();
     output.write(parting);
-  });
+  };
+
+  const handleError = ({ message }) => {
+    output.write(`${message}\n`);
+    rl.close();
+  };
+
+  const handleWrite = (input) => {
+    const writeableInput = input.trim().toLowerCase() === 'exit' ? handleClose() : input;
+    stream.write(`${writeableInput}\n`);
+  };
+
+  rl.write(greeting);
+  rl.on('line', handleWrite);
+  rl.on('SIGINT', handleClose);
+  stream.on('error', handleError);  
 };
 
 readfile('c3poMemory.txt');
